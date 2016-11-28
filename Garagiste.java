a8d78e77b90ce4b1b6af1a57b69f908fcc36c826
ChopShad <http:://192.168.104.206:8080>
2016-11-28 14:33:38 +0100

    nom et adresse

--- Garagiste.java
+++ Garagiste.java
@@ -1,6 +1,14 @@
 
 
 public Class Garagiste{
-
+    private String nom ;
+    private String adresse ;
+    
 }
 
+public Garagiste Garagiste(String nom , String adresse){
+    
+    this.nom = nom ;
+    this.adresse = adresse ;
+    
+}
